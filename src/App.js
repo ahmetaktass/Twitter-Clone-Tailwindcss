26412907b3ccbcf4504daa7fa4b46302a9428782
Ahmet Aktaş <mail.ahmetaktas@gmail.com>
2021-06-24 22:32:44 +0300

    firs commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-purple-600  text-gray-lightest">ahmet</h1>
+    </div>
+  )
+}
+
+export default App
